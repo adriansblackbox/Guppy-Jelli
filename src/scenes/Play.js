a4b58c7c0de5ad65a8474ff7745dbf660d647165
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-04-22 00:26:05 -0700

    Start of the cursor follow
    We need to get a playable scene set up first

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,15 +2,24 @@ class Play extends Phaser.Scene{
     preload(){
         this.load.image('platform','./assets/Ground.png');
         this.load.image('player','./assets/Player.png');
+        //need a sprite for the jelly
     }
     create(){
-        this.platforms = this.add.group({removeCallback: function(platform){platform.scene.platformPool.add(platform)}});
-        this.platformPool = this.add.group({removeCallback: function(platform){platform.scene.platforms.add(platform)}});
-        this.player = new this.player(this,game.config.width/2, game.config.height-borderUISize-boarderPadding);
-        
+        //origionally I started making this work for platforms
+        //this.platforms = this.add.group({removeCallback: function(platform){platform.scene.platformPool.add(platform)}});
+        //this.platformPool = this.add.group({removeCallback: function(platform){platform.scene.platforms.add(platform)}});
+
+        //work in progress
+        //this.player = new this.player(this,game.config.width/2, game.config.height-borderUISize-boarderPadding);
+
+        this.jellyFish = new JellyFish(this,game.config.width + borderUISize * 6, borderUISize*4, 'JellyFish');
+
     }
     update(){
-
+        //will need a bool var to check for gameover
+        //also might work by not putting x/y, but will test once we get a testable game out
+        this.jellyFish.x = this.input.activePointer.x;
+        this.jellyFish.y = this.input.activePointer.y;
     }
     //functions
 
