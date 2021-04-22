8410403813810db991c90f7a0aab18b47212e75a
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-04-21 18:21:59 -0700

    Started coding platforms
    Also added some very basic sprites.
    We will want to consider if we want to code primarely in the scene, or make seperate classes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,13 +1,17 @@
 class Play extends Phaser.Scene{
     preload(){
-
+        this.load.image('platform','./assets/Ground.png');
+        this.load.image('player','./assets/Player.png');
     }
     create(){
-
+        this.platforms = this.add.group({removeCallback: function(platform){platform.scene.platformPool.add(platform)}});
+        this.platformPool = this.add.group({removeCallback: function(platform){platform.scene.platforms.add(platform)}});
+        this.player = new this.player(this,game.config.width/2, game.config.height-borderUISize-boarderPadding);
+        
     }
     update(){
 
     }
     //functions
-    
+
 }
