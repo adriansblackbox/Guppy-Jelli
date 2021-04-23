718e06fe1c87e23eec6dd6fbd3d5db1383691fe4
Adrian <adlvasqu@ucsc.com>
2021-04-23 15:11:26 -0700

    made jelly a physics thing

--- src/prefabs/jellyFish.js
+++ src/prefabs/jellyFish.js
@@ -1,7 +1,8 @@
-class JellyFish extends Phaser.GameObjects.Sprite {
+class JellyFish extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
+        scene.physics.add.existing(this)
         this.speed = 3;
     }
 
